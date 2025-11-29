4a92a6b085211c3187bfcb48f042557ab84a50d6
ghusharib-coder <ghusharibs@gmail.com>
2025-11-29 21:02:16 +0500

    Backend ready for Vercel

--- Server.js
+++ Server.js
@@ -1,13 +1,24 @@
-import express from 'express';
-import cors from 'cors';
-import dbConnect from './dbConnection.js';
-import UserRoutes from './Router.js';
-const PORT=3000;
-const app=express();
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
-app.use(cors());
-app.use("/api/users", UserRoutes);
-dbConnect();
-module.exports = app;
+import express from "express";
+import cors from "cors";
+import dbConnect from "./dbConnection.js";
+import UserRoutes from "./Router.js";
 
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+// Default route (important for Vercel)
+app.get("/", (req, res) => {
+  res.send("Backend is running on Vercel!");
+});
+
+// API routes
+app.use("/api/users", UserRoutes);
+
+// Connect to database
+dbConnect();
+
+// Export for Vercel (ESM)
+export default app;
