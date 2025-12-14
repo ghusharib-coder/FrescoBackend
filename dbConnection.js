b70fcdc9e29a1d0b46906cedfc131e4893de8365
ghusharib-coder <ghusharibs@gmail.com>
2025-12-14 18:56:04 +0500

    Update frontend to use live backend

--- dbConnection.js
+++ dbConnection.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose"
 const dbConnection = async() =>{
-     try{
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('DB Connected');
-    }
-    catch(err){
-        console.log("error",err);
-    }
+      try {
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected ✅");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
 }
 export default dbConnection;
